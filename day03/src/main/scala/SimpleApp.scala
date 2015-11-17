868f5279fd59a15554a547c2895160fb5ece328d
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-11-18 00:13:02 +0700

    refactor code

--- day03/src/main/scala/SimpleApp.scala
+++ day03/src/main/scala/SimpleApp.scala
@@ -5,17 +5,18 @@ import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 
 object SimpleApp {
-	def textSearch(logData: RDD[String]) {
-		val errors = logData.filter(line => line.contains("Mr."))
+	def textSearch(textData: RDD[String]) {
+		val errors = textData.filter(line => line.contains("Mr."))
 		println(errors.count());
 	}
 
 	def main(args: Array[String]) {
-		val logFile = "titanic.csv"
 		val conf = new SparkConf().setAppName("Simple Application")
 		val sc = new SparkContext(conf)
-		val logData = sc.textFile(logFile, 2).cache()
+		
+		//load text file & cache
+		val textData = sc.textFile("titanic.csv").cache()
 
-		textSearch(logData)
+		textSearch(textData)
 	}
 }
